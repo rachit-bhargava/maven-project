275d79e8cbf4780d0494d08174b95a68633e863e
rachit-bhargava <rachit.bhargava@philips.com>
2018-02-21 14:23:46 -0500

    added new property

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -11,10 +12,19 @@ public class HelloMavenTest extends FunctionalTestCase {
 	
 	@Rule
 	public DynamicPort myPort= new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
         System.out.println("\n\n TestCase-1 dynamic Port ------>"+myPort.getNumber());
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      System.out.println("\n\n TestCase-2 dynamic Port ------>"+myPort.getNumber());
+      assertEquals("application/json", contentType);
     }
     
     @Override
